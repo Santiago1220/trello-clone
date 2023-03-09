b059083fd1ac633b5046190b5930a0be9c4bd471
unknown <tiago931220@gmail.com>
2023-03-08 21:44:12 -0500

    Se crean los interceptors y el servicio de user para consumir de la api los usuarios, así mismo se agrega guard para cuidar el acceso a la app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@guards/auth.guard';
+import { RedirectGuard } from '@guards/redirect.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [ RedirectGuard ],
     loadChildren: () => import('./pages/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'app',
+    canActivate: [ AuthGuard ],
     loadChildren: () => import('./pages/layout/layout.module').then((m) => m.LayoutModule),
   },
 ];
--- src/app/pages/users/pages/users-table/users-table.component.ts
+++ src/app/pages/users/pages/users-table/users-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSourceUser } from './data-source';
+import { UsersService } from '@services/users.service';
+import { User } from '@models/user.model';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-users-table',
@@ -6,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersTableComponent implements OnInit {
 
-  constructor() { }
+  dataSource = new DataSourceUser();
+  columns: string[] = ['id', 'avatar', 'name', 'email'];
+  user: User | null = null;
+
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.usersService.getUsers()
+    .subscribe(users => {
+      this.dataSource.init(users);
+    })
+    this.authService.user$
+    .subscribe(user => {
+      this.user = user;
+    })
   }
 
 }
